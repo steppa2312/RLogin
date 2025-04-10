12e2357177aef37f93a3640a4f33d588d95422a1
manzonis98 <63615231+manzonis98@users.noreply.github.com>
2025-04-10 17:10:23 +0200

    ADD eventgrid

---
+++ src/EventGrid.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+// import "./App.css";
+import UserRow from "./UserRow"
+import "./UserGrid.css"; // Assicurati di avere questo file CSS per lo stile della tabella
+const API_URL = process.env.REACT_APP_API_URL;
+
+const UserGrid = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API_URL}/api/users`)
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error("Errore nel caricamento utenti:", err));
+  }, []);
+
+  return (
+    <div className="user-table-container">
+      <table className="user-table">
+        <thead>
+          <tr>
+            <th>Nome evento</th>
+             <th>Descrizione</th>
+             <th>Data</th>
+             <th>Luogo</th>
+              <th>Azioni utente</th>
+          </tr>
+        </thead>
+        <tbody>
+        {users.map((user, index) => (
+             <tr key={user.username || index}>
+               <td>{user.nome}</td>
+               <td>{user.username}</td>
+               <td>{user.ruolo}</td>
+               <td>
+                 <button className="action-btn">Modifica</button>
+                 <button className="action-btn delete">Elimina</button>
+               </td>
+             </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default UserGrid;
